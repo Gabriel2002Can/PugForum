192756a40582c8fecf99b10bdd2eb87a68b6a294
Gabriel2002Can <gabriel2002ca@gmail.com>
2025-01-28 20:10:04 -0400

    Models done

---
+++ PugForum/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualBasic;
+
+namespace PugForum.Models
+{
+    public class Comment
+    {
+        int CommentId { get; set; }
+        string Content { get; set; } = string.Empty;
+        DateAndTime CreateDate { get; set; }
+        int DiscussionId { get; set; }
+        Discussion? Discussion { get; set; }
+
+    }
+}
---
+++ PugForum/Models/Discussion.cs
@@ -0,0 +1,13 @@
+﻿namespace PugForum.Models
+{
+    public class Discussion
+    {
+        int DiscussionId { get; set; }
+        string Title { get; set; } = string.Empty;
+        string Content { get; set; } = string.Empty;
+        string ImageFileName { get; set; } = string.Empty;
+        DateTime CreateDate { get; set; }
+        List<Comment>? Comments { get; set; }
+
+    }
+}
